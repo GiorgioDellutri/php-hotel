7ac466cfb4d622a102ad14337a86c4a8cd0e78b6
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-25 19:03:44 +0100

    added control.php with parking check

---
+++ control.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Import bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <main>
+        <table class="table table-dark">
+            <?php
+
+            $hotels = [
+                [
+                    'name' => 'Hotel Belvedere',
+                    'description' => 'Hotel Belvedere Descrizione',
+                    'parking' => true,
+                    'vote' => 4,
+                    'distance_to_center' => 10.4
+                ],
+                [
+                    'name' => 'Hotel Futuro',
+                    'description' => 'Hotel Futuro Descrizione',
+                    'parking' => true,
+                    'vote' => 2,
+                    'distance_to_center' => 2
+                ],
+                [
+                    'name' => 'Hotel Rivamare',
+                    'description' => 'Hotel Rivamare Descrizione',
+                    'parking' => false,
+                    'vote' => 1,
+                    'distance_to_center' => 1
+                ],
+                [
+                    'name' => 'Hotel Bellavista',
+                    'description' => 'Hotel Bellavista Descrizione',
+                    'parking' => false,
+                    'vote' => 5,
+                    'distance_to_center' => 5.5
+                ],
+                [
+                    'name' => 'Hotel Milano',
+                    'description' => 'Hotel Milano Descrizione',
+                    'parking' => true,
+                    'vote' => 2,
+                    'distance_to_center' => 50
+                ],
+            ];
+
+            $posto = $_GET['posto'];
+
+            // if ($posto === "yes") {
+            //     $posto = true;
+            // } elseif ($posto === "no") {
+            //     $posto = false;
+            // }
+
+            foreach ($hotels as $hotel) {
+                if ($hotel['parking'] === true && $posto === 'yes') {
+                    echo "<tr>";
+                    echo "<td>" . $hotel['name'] . "</td>";
+                    echo "<td>" . $hotel['description'] . "</td>";
+                    echo "<td>" . $hotel['parking'] . "</td>";
+                    echo "<td>" . $hotel['vote'] . "</td>";
+                    echo "<td>" . $hotel['distance_to_center'] . " Km" . "</td>";
+                    echo "</tr>";
+                } elseif ($hotel['parking'] === false && $posto === 'no') {
+                    echo "<tr>";
+                    echo "<td>" . $hotel['name'] . "</td>";
+                    echo "<td>" . $hotel['description'] . "</td>";
+                    echo "<td>" . $hotel['parking'] . "</td>";
+                    echo "<td>" . $hotel['vote'] . "</td>";
+                    echo "<td>" . $hotel['distance_to_center'] . " Km" . "</td>";
+                    echo "</tr>";
+                }
+            }
+
+            // function avaibleParking($parking)
+            // {
+            //     if ($parking['parking'] === true) {
+            //         return "yes";
+            //     } else {
+            //         return "no";
+            //     }
+            // }
+
+            ?>
+        </table>
+
+    </main>
+</body>
+
+</html>
